0b40b7b0551706da0385ea873a1e17f2a241ae95
yuanguoyao <heizi.ygy@raycloud.com>
2019-01-15 16:50:45 +0800

    feat: 判断回文链表，运用了链表反转

---
+++ src/main/java/com/ygy/liberal/arithmetic/list/ReverseNodeString.java
@@ -0,0 +1,60 @@
+package com.ygy.liberal.arithmetic.list;
+
+import com.ygy.liberal.arithmetic.SingleNode;
+
+/**
+ * Created by guoyao on 2019/1/15.
+ * 回文字符串的问题
+ */
+public class ReverseNodeString {
+
+    public static void main(String[] args) {
+        SingleNode doubleNode=createDoubleNode();
+        System.out.println(doubleNode.length());
+
+        System.out.println(checkIsCanReverse(doubleNode));
+    }
+
+    private static boolean checkIsCanReverse(SingleNode doubleNode) {
+        int index = 0;
+        SingleNode slowNode = doubleNode;
+        SingleNode fastNode = doubleNode;
+        while (index < doubleNode.length()/2) {
+            slowNode=slowNode.next();
+            fastNode=fastNode.next().next();
+            index++;
+        }
+        slowNode=reverseNode(slowNode);
+        fastNode=doubleNode;
+        while (slowNode != null) {
+            if (fastNode.value() != slowNode.value()) {
+                return false;
+            }
+            slowNode=slowNode.next();
+            fastNode=fastNode.next();
+        }
+        return true;
+    }
+
+    private static SingleNode reverseNode(SingleNode baseNode) {
+        //反转node
+        SingleNode temp = baseNode;
+        SingleNode prev = null;
+        while (baseNode != null) {
+            temp=baseNode.next();
+            baseNode.next(prev);
+            prev=baseNode;
+            baseNode = temp;
+        }
+        return prev;
+    }
+
+
+
+    private static SingleNode createDoubleNode() {
+        return new SingleNode(0,
+                        new SingleNode(2,
+                                        new SingleNode(1,
+                                                new SingleNode(0, null))));
+    }
+}
