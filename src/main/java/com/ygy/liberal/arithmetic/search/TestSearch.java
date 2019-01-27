60c7c94b8e37c168af652d9832fd5767edd165ea
yuanguoyao <heizi.ygy@raycloud.com>
2019-01-27 15:19:49 +0800

    feat: 二分查找第一个或最后一个等于指定数据

--- src/main/java/com/ygy/liberal/arithmetic/search/TestSearch.java
+++ src/main/java/com/ygy/liberal/arithmetic/search/TestSearch.java
@@ -1,6 +1,6 @@
 package com.ygy.liberal.arithmetic.search;
 
-import com.ygy.liberal.arithmetic.search.binary.BinarySearch;
+import com.ygy.liberal.arithmetic.search.binary.BinaryFirstOrLastEqualSerch;
 
 /**
  * Created by guoyao on 2019/1/27.
@@ -8,12 +8,38 @@ import com.ygy.liberal.arithmetic.search.binary.BinarySearch;
 public class TestSearch {
 
     public static void main(String[] args) {
-        System.out.println(new BinarySearch().searchTheNum(createDatas(), -1));
-        System.out.println(new BinarySearch().searchTheNumNeedInsert(createDatas(), 0));
+        //System.out.println(new BinarySearch().searchTheNum(createDatas(), -1));
+        //System.out.println(new BinarySearch().searchTheNumNeedInsert(createDatas(), 0));
+        //System.out.println(new BinaryFirstOrLastEqualSerch().BinaryFirstEqualSerch(createDatas(), 7));
+        //System.out.println(new BinaryFirstOrLastEqualSerch().BinaryFirstEqualSerch(createDatas1(), 7));
+        //System.out.println(new BinaryFirstOrLastEqualSerch().BinaryFirstEqualSerch(createDatas2(), 7));
+        //System.out.println(new BinaryFirstOrLastEqualSerch().BinaryFirstEqualSerch(createDatas3(), 7));
+        //System.out.println(new BinaryFirstOrLastEqualSerch().BinaryFirstEqualSerch(createDatas4(), 7));
+        System.out.println(new BinaryFirstOrLastEqualSerch().BinaryLastEqualSerch(createDatas(), 7));
+        System.out.println(new BinaryFirstOrLastEqualSerch().BinaryLastEqualSerch(createDatas1(), 7));
+        System.out.println(new BinaryFirstOrLastEqualSerch().BinaryLastEqualSerch(createDatas2(), 7));
+        System.out.println(new BinaryFirstOrLastEqualSerch().BinaryLastEqualSerch(createDatas3(), 7));
+        System.out.println(new BinaryFirstOrLastEqualSerch().BinaryLastEqualSerch(createDatas4(), 7));
     }
 
     private static int[] createDatas() {
-        return new int[]{1};
+        return new int[]{1,2,7,7,7,88};
+    }
+
+    private static int[] createDatas1() {
+        return new int[]{1,2,3,7,7,7,88};
+    }
+
+    private static int[] createDatas2() {
+        return new int[]{1,2,3,4,7,7,7,88};
+    }
+
+    private static int[] createDatas3() {
+        return new int[]{1,2,3,4,5,7,7,7,88};
+    }
+
+    private static int[] createDatas4() {
+        return new int[]{1,2,3,4,5,6,7,7,7,88};
     }
 
 }
---
+++ src/main/java/com/ygy/liberal/arithmetic/search/binary/BinaryFirstOrLastEqualSerch.java
@@ -0,0 +1,52 @@
+package com.ygy.liberal.arithmetic.search.binary;
+
+/**
+ * Created by guoyao on 2019/1/27.
+ */
+public class BinaryFirstOrLastEqualSerch {
+
+
+    public int BinaryFirstEqualSerch(int[] datas, int theNum) {
+        int start=0;
+        int end=datas.length - 1;
+        while (start <= end) {
+            int mid=start + ((end - start) >> 1);
+            if (datas[mid] == theNum) {
+                while (--mid >= 0) {
+                    if (datas[mid] != theNum) {
+                        mid=mid + 1;
+                        break;
+                    }
+                }
+                return mid;
+            } else if (datas[mid] > theNum) {
+                end=mid - 1;
+            } else if (datas[mid] < theNum) {
+                start=mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public int BinaryLastEqualSerch(int[] datas, int theNum) {
+        int start=0;
+        int end=datas.length - 1;
+        while (start <= end) {
+            int mid=start + ((end - start) >> 1);
+            if (datas[mid] == theNum) {
+                while (++mid <= end) {
+                    if (datas[mid] != theNum) {
+                        mid=mid - 1;
+                        break;
+                    }
+                }
+                return mid;
+            } else if (datas[mid] > theNum) {
+                end=mid - 1;
+            } else if (datas[mid] < theNum) {
+                start=mid + 1;
+            }
+        }
+        return -1;
+    }
+}
